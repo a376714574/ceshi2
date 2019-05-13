381ccadb0943a055ef4f136683dcd86ff1c7b285
a376714574 <376714574@qq.com>
2019-05-13 22:11:38 +0800

    新的增加

--- new/run.py
+++ new/run.py
@@ -1,11 +1,12 @@
 import unittest
 import sys
 
-sys.path.append("./")
-
-from new import HQ
-
-a=unittest.TestSuite()
-a.addTest(unittest.TestLoader().loadTestsFromModule(HQ))
-unittest.TextTestRunner().run(a)
-
+sys.path.append("././")
+print(sys.path)
+#
+# from new import HQ
+#
+# a=unittest.TestSuite()
+# a.addTest(unittest.TestLoader().loadTestsFromModule(HQ))
+# unittest.TextTestRunner().run(a)
+#
