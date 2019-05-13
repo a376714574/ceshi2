296adda3430c5ad21283105da604c9e78314ce10
a376714574 <376714574@qq.com>
2019-05-13 23:19:23 +0800

    新的增加

--- new/config/os_path.py
+++ new/config/os_path.py
@@ -4,10 +4,12 @@ a=os.path
 
 excel_old=a.dirname(a.dirname(a.realpath("cases.xlsx")))
 
-excel_new=a.join(excel_old,"new","ck_list","cases.xlsx")
+excel_new=a.join(excel_old,"python15","new","ck_list","cases.xlsx")
 
+print(excel_new)
 
 config_old=a.dirname(a.dirname(a.realpath("config.conf")))
 
-config_new=a.join(excel_old,"new","config","config.conf")
+config_new=a.join(excel_old,"python15","new","config","config.conf")
 
+print(config_new)
