50130cae01f673522ede3ba43cefff6ff7c1ac0a
a376714574 <376714574@qq.com>
2019-05-13 23:31:05 +0800

    新的增加

--- new/run.py
+++ new/run.py
@@ -1,4 +1,5 @@
 import unittest
+import  HTMLTestRunnerNew
 import sys
 
 sys.path.append("././")
@@ -6,7 +7,9 @@ print(sys.path)
 
 from new import HQ
 
-a=unittest.TestSuite()
-a.addTest(unittest.TestLoader().loadTestsFromModule(HQ))
-unittest.TextTestRunner().run(a)
+with open("test.html","wb") as file:
+    a=unittest.TestSuite()
+    a.addTest(unittest.TestLoader().loadTestsFromModule(HQ))
+    # unittest.TextTestRunner().run(a)
+    HTMLTestRunnerNew.HTMLTestRunner(stream=file).run(a)
 
